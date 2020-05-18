4e40ebdea1a522bf89eb4387721e089ff238386c
akshay <akshayg.nemesis@gmail.com>
2020-05-18 16:19:56 +0530

    *

--- codedaddies_list/settings.py
+++ codedaddies_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -129,3 +130,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
+django_heroku.settings(locals())
